70b93f004ac7514c0b3aa2e0e9e168b6c77bd139
cube0x0 <39370848+cube0x0@users.noreply.github.com>
2019-08-02 20:19:12 +0200

    Add files via upload

--- Testing/Invoke-WinEnum.ps1
+++ Testing/Invoke-WinEnum.ps1
@@ -1,3 +1,97 @@
+function Get-FirewallStatus {
+    #https://github.com/threatexpress/red-team-scripts/blob/master/HostEnum.ps1
+    $regkey = "HKLM:\System\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy"
+    $output = New-Object -TypeName PSobject -Property @{
+        Standard    = If ((Get-ItemProperty $regkey\StandardProfile).EnableFirewall -eq 1){"Enabled"}Else {"Disabled"}
+        Domain      = If ((Get-ItemProperty $regkey\DomainProfile).EnableFirewall -eq 1){"Enabled"}Else {"Disabled"}
+        Public      = If ((Get-ItemProperty $regkey\PublicProfile).EnableFirewall -eq 1){"Enabled"}Else {"Disabled"}
+    }
+    Write-Output $output
+}
+function Get-LocalShares {
+    <#
+    Modified https://github.com/A-mIn3/WINspect/blob/master/WINspect.ps1
+    #>
+    $permissionFlags = @{
+        0x1 =   "Read-List";
+        0x2 =   "Write-Create";
+        0x4 =   "Append-Create Subdirectory";                  	
+        0x20    =   "Execute file-Traverse directory";
+        0x40    =   "Delete child"
+        0x10000 =   "Delete";                     
+        0x40000 =   "Write access to DACL";
+        0x80000 =   "Write Onwer"
+    }
+    $shares = New-Object System.Collections.ArrayList
+    try{
+        Get-WmiObject -class Win32_share -Filter "type=0"| foreach {
+            $shareSecurityObj = Get-WmiObject -class Win32_LogicalShareSecuritySetting -Filter "Name='$($_.Name)'"
+            $securityDescriptor = $shareSecurityObj.GetSecurityDescriptor().Descriptor
+            ForEach($ace in $securityDescriptor.dacl){
+                # 0 = Allow ; 1 = Deny
+                if([int]$ace.acetype -eq 0){
+                    $accessMask  = $ace.accessmask
+                    $permissions = ""
+                    foreach($flag in $permissionFlags.Keys){
+                        if($flag -band $accessMask){
+                            $permissions+=$permissionFlags[$flag]
+                            $permissions+=";"
+                        }
+                    }
+                    $share = New-Object  PSObject -Property @{
+                        "ShareName"    =  $_.Name     
+                        "Trustee"      =  $ace.trustee.Name 
+                        "Permissions"  =  $permissions
+                    }
+                    $shares.add($share) | Out-Null
+                }
+            }    
+        }
+    }catch{
+        Write-Output "[-] $($_.Exception.Message)"
+    }
+    if($shares.Count -gt 0){
+        Write-Output ($shares | Format-List ShareName,Trustee, Permissions)
+    }else{
+        Write-Output "[*] No local Shares Were Found"
+    }
+}
+function Get-UACLevel {
+    <#
+    https://github.com/A-mIn3/WINspect/blob/master/WINspect.ps1
+    .SYNOPSIS
+    Checks current configuration of User Account Control
+    .DESCRIPTION
+    This functions inspects registry informations related to UAC configuration 
+    and checks whether UAC is enabled and which level of operation is used.
+    #>
+    try {
+        $UACRegValues = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+        if ([int]$UACRegValues.EnableLUA -eq 1) {
+            Write-Output "[+] UAC is enabled"
+        }
+        else {
+            Write-Output "[-] UAC is disabled"
+        }
+        $consentPrompt = $UACregValues.ConsentPromptBehaviorAdmin
+        $secureDesktop = $UACregValues.PromptOnSecureDesktop
+        if ( $consentPrompt -eq 0 -and $secureDesktop -eq 0) {
+            Write-Output "[*] UAC Level : Never Notify"
+        }
+        elseif ($consentPrompt -eq 5 -and $secureDesktop -eq 0) {
+            Write-Output "[*] UAC Level : Notify only when apps try to make changes (No secure desktop)"
+        }
+        elseif ($consentPrompt -eq 5 -and $secureDesktop -eq 1) {
+            Write-Output "[*] UAC Level : Notify only when apps try to make changes (secure desktop on)"
+        }
+        elseif ($consentPrompt -eq 5 -and $secureDesktop -eq 2) {
+            Write-Output "[*] UAC Level : Always Notify with secure desktop"
+        }
+    }
+    catch {
+        Write-Output "[-] $($_.Exception.Message)"
+    }
+}
 function Invoke-WinEnum{
     Write-Output "[*] ComputerName $env:COMPUTERNAME"
     Write-Output "[*] User $env:USERNAME"
@@ -58,4 +152,43 @@ function Invoke-WinEnum{
     }catch{
         Write-Output "[-] Unquoted Services Failed"
     }
+
+    #https://powersploit.readthedocs.io/en/latest/Privesc/Get-RegistryAlwaysInstallElevated/
+    Write-Output "`n[*] Checking AlwaysInstallElevated"
+    try{
+        $UnquotedService=Get-RegistryAlwaysInstallElevated
+        if($UnquotedService){
+            Write-Output "[-] AlwaysInstallElevated Found"
+            $UnquotedService
+        }else{
+            Write-Output "[+] No AlwaysInstallElevated Found"
+        }
+    }catch{
+        Write-Output "[-] AlwaysInstallElevated Failed"
+    }
+
+    #
+    Write-Output "`n[*] Checking Firewall Status"
+    try{
+        Get-FirewallStatus
+    }catch{
+        Write-Output "[-] Firewall status Failed"
+    }
+
+    #
+    Write-Output "`n[*] Checking for UAC Configuration"
+    try{
+        Get-UACLevel
+    }catch{
+        Write-Output "[-] Checking for UAC Configuration Failed"
+    }
+
+    #
+    Write-Output "`n[*] Checking for Local Shares"
+    try{
+        Get-LocalShares
+    }catch{
+        Write-Output "[-] Checking for Local Shares Failed"
+    }
 }
+Invoke-WinEnum
